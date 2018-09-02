562b15a3d6c96f817e98bde1bd0c9eb0920afa62
Dan Hintz <dhintz@live.com>
2018-09-02 21:42:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,10 +35,8 @@ class Song
 
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) == nil
-      puts "positive"
       self.create_by_name(name)
     else
-      puts "negative"
       self.find_by_name(name)
     end
   end
