284d2b502b17eba086187b7b98730d8f9c3f7aea
Dan Hintz <dhintz@live.com>
2018-09-02 21:33:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,13 +33,13 @@ class Song
     self.all.detect {|song| song.name == name}
   end
 
-  # def self.find_or_create_by_name(name)
-  #   if self.all.include?(name)
-  #     self.find_by_name(name)
-  #   else 
-  #     self.create_by_name(name)
-  #   end
-  # end
+  def self.find_or_create_by_name(name)
+    if self.all.include?(name)
+      self.find_by_name(name)
+    else 
+      self.create_by_name(name)
+    end
+  end
     
 end
 
